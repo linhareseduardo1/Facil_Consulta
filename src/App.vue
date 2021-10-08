a7d5c520037439766c18f5ac03596778db782aa6
linhareseduardo1 <70285912+linhareseduardo1@users.noreply.github.com>
2021-10-08 03:38:33 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,123 @@
+<template>
+  <div id="app" class="">
+    <PageAtendimento />
+  </div>
+</template>
+
+
+<script>
+import PageAtendimento from "./components/PageAtendimento.vue";
+
+export default {
+  components: {
+    PageAtendimento: PageAtendimento,
+  },
+};
+</script>
+<!--
+
+<script>
+import PageHome from "./components/PageHome.vue";
+
+export default {
+  components: {
+    PageHome: PageHome,
+  },
+};
+</script>
+-->
+
+<!--
+<script>
+import PageRevisao from "./components/PageRevisao.vue";
+
+export default {
+  components: {
+    PageHome: PageRevisao,
+  },
+};
+</script>
+
+-->
+
+<style lang="scss">
+$primary-0: #483698;
+$secondary-0: #ffffff;
+$secondary-1: #f9f9f9;
+$secondary-3: #b9b9b9;
+$secondary-4: #8a8a8a;
+$secondary-7: #282828;
+$cta-1: #ffe766;
+$cta-0: #fbde40;
+$danger: #dc3545; //
+$transparent: none;
+
+@import url("https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;1,300&display=swap");
+
+.row {
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+}
+
+h1,
+h1.bold {
+  color: $primary-0;
+  font-family: "Comfortaa", cursive;
+}
+h2,
+h3,
+h4 {
+  color: $primary-0;
+  font-family: "Open Sans";
+}
+</style>
+
+<style>
+#app {
+  font-family: "Confortaa";
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+label {
+  font-family: "Open Sans";
+}
+
+.raised-block {
+  background-color: #fff;
+  margin-bottom: 10px;
+  margin-left: 0;
+  margin-right: -0.625rem;
+  padding-left: 0.625rem;
+  padding-right: 0.625rem;
+}
+@media (max-width: 33.9em) {
+  .raised-block {
+    margin-left: -0.625rem;
+  }
+}
+.row [class^="col-"]:first-child > .raised-block {
+  margin-left: -0.625rem;
+}
+*,
+*:after,
+*:before {
+  margin: 0;
+  padding: 0;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+}
+
+img {
+  margin: 1 em;
+  width: auto;
+  max-width: 100%;
+  height: auto !important;
+}
+</style>
---
+++ src/components/PageHome.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="hello">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 rounded px-5 py-4 shadow bg-white text-left">
+          <h1>Sobre o profissional</h1>
+          <h2>Dados do profissional</h2>
+          <div id="header"></div>
+          <form id="home" @submit="checkForm" action="#" method="post">
+            <!--
+    <p v-if="errors.length">
+      <b>Por favor, corrija o(s) seguinte(s) erro(s):</b>
+      <ul>
+        <li v-for="error in errors">{{ error }}</li>
+      </ul>
+    </div> -->
+            <div class="form-group">
+              <label for="name">Nome completo* </label>
+              <br />
+              <input
+                id="name"
+                v-model="name"
+                class="form-control"
+                type="text"
+                name="name"
+                placeholder="Digite o nome completo"
+              />
+            </div>
+
+            <div class="form-group">
+              <label for="age">CPF* </label><br />
+              <input
+                id="cpf"
+                v-model="cpf"
+                class="form-control"
+                type="cpf"
+                name="cpf"
+                min="9"
+                max="9"
+                placeholder="Digite um CPF"
+              />
+            </div>
+
+            <div class="form-group">
+              <label for="tel">Número de celular* </label><br />
+              <input
+                id="tel"
+                v-model="phone"
+                class="form-control"
+                type="cpf"
+                name="cpf"
+                min="9"
+                max="9"
+                placeholder="(00) 0 0000-0000"
+              />
+            </div>
+            <div class="form-group">
+              <label for="estado">Estado* </label><br />
+              <select
+                id="estado"
+                v-model="estado"
+                class="form-control"
+                name="estado"
+              >
+                <option>Paraná</option>
+                <option>Rio Grande do Sul</option>
+                <option>Santa Catarina</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label for="cidade">Cidade* </label><br />
+              <select
+                id="cidade"
+                v-model="cidade"
+                class="form-control"
+                name="estado"
+              >
+                <option>Londrina(PR)</option>
+                <option>Maringá(PR)</option>
+                <option>Pelotas(RS)</option>
+                <option>Porto Alegre(RS)</option>
+                <option>Florianópolis(SC)</option>
+                <option>Joinville (RS-)</option>
+              </select>
+            </div>
+            <br />
+            <div class="form-group">
+              <input type="submit" class="form-control" value="PRÓXIMO" />
+            </div>
+          </form>
+        </div>
+        <div class="imagem vue-bg col-md-4 d-flex align-items-center">
+          <img alt="Vue logo" src="../assets/desktop-pagina-1.png" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default { name: "PageHome" };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+$primary-0: #483698;
+
+h1 {
+  color: $primary-0;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+</style>
+
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
